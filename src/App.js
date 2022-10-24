38524fc95ac869d8fd4cfea7f4a26b4bcbae3745
Benigne <ikiben23@gmail.com>
2022-10-24 16:50:37 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+function App(e) {
+   const[fname,setfname]=useState('');
+   const[lname,setlname]=useState('');
+   
+   
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='fname'>
+        <label >FirstName:</label>
+        <input type="text" name="Firstname"placeholder="FName" onChange={e=>setfname(e.target.value)}/>
+        </div>
+        <div className='lname'>
+        <label>LastName:</label>
+        <input type="text" name="Lastname"placeholder="LName" onChange={e=>setlname(e.target.value)} />
+        </div>
+    </header>
+   <div className='text-output'>
+   <h1>FirstName:{fname}</h1>
+   <h1>LastName:{lname}</h1>
+   </div>
     </div>
   );
 }
